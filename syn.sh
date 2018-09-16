34a1f0c0ee72f65f0e21a0545fcf2e9ada9e4f89
Christian Haettich <feddischson@gmx.com>
2018-09-16 08:17:08 +0200

    Adds Makefile and flash targets

--- syn.sh
+++
@@ -1,8 +0,0 @@
-quartus_sh --flow compile de0_led_example.qpf
-echo =====================================
-echo
-echo Done, do something like
-echo   \> quartus_pgm de0_led_example.cdf   
-echo to download the example to the FPGA
-echo
-echo =====================================
